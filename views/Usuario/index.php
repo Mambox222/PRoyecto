79ce42dddb10e13a56fccb4cc8b24350d5d9abcf
Mambox222 <122553718+Mambox222@users.noreply.github.com>
2023-06-06 00:50:16 -0400

    Poyecto progra web

---
+++ views/Usuario/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <?php require_once("../html/mainhead.php");?>
+    <title>Univalle:Home</title>
+  </head>
+
+  <body>
+  <?php require_once("../html/mainmenu.php");?>
+  <?php require_once("../html/mainheader.php");?>
+    
+
+    <!-- ########## START: MAIN PANEL ########## -->
+    <div class="br-mainpanel">
+      <div class="br-pageheader pd-y-15 pd-l-20">
+        <nav class="breadcrumb pd-0 mg-0 tx-12">
+          <a class="breadcrumb-item" href="#">Home</a>
+        </nav>
+      </div><!-- br-pageheader -->
+      <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
+        <h4 class="tx-gray-800 mg-b-5">Home</h4>
+        <p class="mg-b-0">Pantalla Home</p>
+      </div>
+
+      <div class="br-pagebody">
+
+        <!-- start you own content here -->
+
+      </div><!-- br-pagebody -->
+
+    </div><!-- br-mainpanel -->
+    <!-- ########## END: MAIN PANEL ########## -->
+
+    <?php require_once("../html/mainjs.php");?>
+
+    
+  </body>
+</html>
